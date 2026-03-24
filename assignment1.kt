941414274b611e59e88797f84ef104af662bbfe5
Drain254 <draindominic506@gmail.com>
2026-03-24 16:46:28 +0300

    go

--- assignment1.kt
+++ assignment1.kt
@@ -1,7 +1,7 @@
 // simple interest
 fun main(){
    val principal : Int = 2000
-    val rate : Int = 23
+    val rate : Int = 25
     val time : Int = 2
     val interest : Int = principal * rate * time /100
     println("The answer is $interest")
